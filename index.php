745c43d5d26bc2a40d18f88a184f31cdd268e554
Davis Kemlers <daviskemlers@gmail.com>
2015-10-10 14:25:19 +0900

    拡張子をphpに変更、includeを使って、DRYにする

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="jquery-1.11.3.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/main.css" media="all">
+    <title>OIC本屋</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+
+    <?php include("includes/header.html"); ?>
+    <?php include("includes/navigation.html"); ?>
+    <?php include("includes/sidebar.html"); ?>
+    <main>
+      <div id="advertisement-tab">広告一覧</div>
+      <div id="new-arrivals-tab">新着商品一覧</div>
+    </main>
+    <?php include("includes/footer.html"); ?>
+  </body>
+</html>
---
+++ search_results.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <script src="jquery-1.11.3.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/main.css" media="all">
+    <title>検索結果</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+
+    <?php include("includes/header.html"); ?>
+    <?php include("includes/navigation.html"); ?>
+    <?php include("includes/sidebar.html"); ?>
+    <main>
+
+    </main>
+    <?php include("includes/footer.html"); ?>
+  </body>
+</html>
