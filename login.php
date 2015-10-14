b45a9309b328ef39f6a97f6a72450b055779ffbb
Davis Kemlers <daviskemlers@gmail.com>
2015-10-14 14:42:27 +0900

    login css, php ファイル追加

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/main.css" media="all">
+    <link rel="stylesheet" type="text/css" href="css/login.css" media="all">
+  <title>OICBooks</title>
+</head>
+<body>
+  <?php include("includes/sidebar.html"); ?>
+  <?php include("includes/header.html"); ?>
+  <main>
+    <input id = "login">
+    <p>aaaaaaaaaaaaaaaa</p>
+  </main>
+  <?php include("includes/footer.html"); ?>
+
+</body>
+</html>
