2c89b3be53033771f263784d32953d2aca88ea39
Davis Kemlers <daviskemlers@gmail.com>
2015-10-16 19:04:53 +0900

    adminページ用意した

---
+++ admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/main.css" media="all">
+  <link rel="stylesheet" type="text/css" href="css/top_page.css" media="all">
+  <title>OICBooksへようこそ！</title>
+</head>
+<body>
+  <?php include("includes/sidebar.html"); ?>
+  <?php include("includes/header.html"); ?>
+  <main>
+    <!--ここで入力-->
+  </main>
+  <?php include("includes/footer.html"); ?>
+</body>
+</html>
