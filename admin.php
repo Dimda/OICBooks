755d487f86278b452d6b9855f9ae92ec591b3897
Davis Kemlers <daviskemlers@gmail.com>
2015-10-20 21:36:17 +0900

    検索結果サイズ調整

--- admin.php
+++ admin.php
@@ -29,7 +29,39 @@
   </div>
   <main>
     <div id="product-manager">
+      <ul class="search-results">
+        <?php
+          $servername = "localhost";
+          $username = "root";
+          $password = "cocacola";
+          $dbname = "oicbooks";
+
+          $conn = new mysqli($servername, $username, $password, $dbname);
+
+          if($conn->connect_error){
+            die("接続失敗" . $conn->connect_error);
+          }
+          if (!$conn->set_charset("utf8")) {
+              exit();
+          }
+
+          $sql = "SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_PRICE FROM PRODUCT";
+          $result = $conn->query($sql);
+          echo '<table>
+                  <th>画像</th><th>商品名</th><th>価格</th>';
+          while($row = $result->fetch_assoc()){
+              echo '<tr>
+                        <td rowspan="1" width= 20% ><img class="product-picture" src="product_image/' . $row["PRODUCT_ID"] .'.jpg"  width="auto" height="200"></td>
+                        <td><a class = "product-name" href="">' . $row["PRODUCT_NAME"] .'</a></td>
+                        <td class = "product-price">' . $row["PRODUCT_PRICE"] .'円</td>
+                      </tr>';
+                 }
+              echo '</table>';
+              $conn->close();
+            ?>
+      </ul>
     </div>
+
   </main>
 </body>
 </html>
--- search_results.php
+++ search_results.php
@@ -12,7 +12,7 @@
    <?php include("includes/header.html"); ?>
    <?php include("includes/sidebar.html"); ?>
    <main>
-     <ul class="search-results">
+     <ul id="search-results">
        <?php
          $servername = "localhost";
          $username = "root";
@@ -43,10 +43,10 @@
          echo '<table>
                  <th>画像</th><th>商品名</th><th>価格</th>';
          while($row = $result->fetch_assoc()){
-             echo '<tr>
-                       <td rowspan="1" width= 20% ><img class="product-picture" src="product_image/' . $row["PRODUCT_ID"] .'.jpg"  width="auto" height="200"></td>
-                       <td><a class = "product-name" href="">' . $row["PRODUCT_NAME"] .'</a></td>
-                       <td class = "product-price">' . $row["PRODUCT_PRICE"] .'円</td>
+             echo   '<tr>
+                       <td class = "book-image" width = "20%"><img class="product-picture" src="product_image/' . $row["PRODUCT_ID"] .'.jpg"  width="auto" height="200px"></td>
+                       <td class = "product-name" width = "70%"><a class = "product-name" href="">' . $row["PRODUCT_NAME"] .'</a></td>
+                       <td class = "product-price" width = "10%">' . $row["PRODUCT_PRICE"] .'円</td>
                      </tr>';
                 }
              echo '</table>';
