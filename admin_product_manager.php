5087127be4a4a5b6d384c0d339dbbb917c11fb51
Davis Kemlers <daviskemlers@gmail.com>
2015-10-27 14:16:02 +0900

    a

--- admin_product_manager.php
+++ admin_product_manager.php
@@ -2,7 +2,7 @@
 
 <div id="product-manager">
   <h2>商品の編入、削除</h2>
-  <form action="admin.php?searchClicked=true" method="post">
+  <form action="admin_product_manager.php?searchClicked=true" method="post">
     <!--searchClickedで検索したってことを伝える-->
     <input id="search_db" placeholder="データベースの中を検索する" name="keyword">
     <input id="search_btn" type="submit" value="検索">
@@ -13,7 +13,7 @@
         include("includes/connect_DB.php");
         $keyword = $_POST["keyword"];
         $keyword = mysqli_real_escape_string($conn, $keyword);
-        echo $keyword . "の検索結果";
+        echo "[ " . $keyword . " ]の検索結果";
 
         if($keyword == ""){
           $sql = "SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_PRICE FROM PRODUCT";
@@ -29,14 +29,20 @@
                       <td class = "product-name" width = "70%"><a class = "product-name" href="product_details.php?id=' . $row["PRODUCT_ID"] . '">' . $row["PRODUCT_NAME"] .'</a></td>
                       <td class = "product-price" width = "10%">' . $row["PRODUCT_PRICE"] .'円</td>
                       <td class = "edit" width = "10%"><a href="">編集</a></td>
-                      <td class = "delete" width = "10%"><a href="delete.php?ID=' . $row["PRODUCT_ID"] . '">削除</a></td>
+                      <td class = "delete" width = "10%"><a onclick="myFunction()" href="delete.php?ID=' . $row["PRODUCT_ID"] . '">削除</a></td>
                     </tr>';
                }
             echo '</table>';
             $conn->close();
       }
       ?>
-
+    <script>
+      function myFunction() {
+        if(confirm("Press a button!") == true){
+          /*window.location = "delete.php?ID=' . $row["PRODUCT_ID"] . '";*/
+        }
+      }
+    </script>
   </ul>
 </div>
 
