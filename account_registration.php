06952552a9fe249824844d788e315d420cb55ffd
Tyokopai <take353@gmail.com>
2015-11-10 16:19:13 +0900

    re-coding account_registration

--- account_registration.php
+++ account_registration.php
@@ -1,111 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="CSS/main.css">
-	<link rel="stylesheet" type="text/css" href="CSS/main_color.css">
-	<link rel="stylesheet" type="text/css" href="CSS/account_refistration.css">
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="http://jpostal.googlecode.com/svn/trunk/jquery.jpostal.js"></script>
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<meta charset="utf-8">
 	<title>アカウント登録</title>
-	<script type="text/javascript">
-   $(window).ready( function() {
-      $('#address1').jpostal({
-         postcode : [
-            '#address1',
-            '#address2'
-         ],
-         address : {
-            '#pref'  : '%3',
-            '#city'  : '%4',
-            '#add' : '%5'
-         }
-      });
-   });
-</script>
 </head>
 <body>
-	<?php include("includes/sidebar.html"); ?>
-	<?php include("includes/header.html"); ?>
-	<main>
-		<form>
-			<table>
-				<tr>
-					<th>名前</th>
-					<td>
-						姓 <input type="text" id="surname" name="surname" maxlength="20">
-						名 <input type="text" id="name" name="name" maxlength="20">
-					</td>
-				</tr>
-				<tr>
-					<th>フリガナ</th>
-					<td>
-						セイ <input type="text" id="phonetic" name="phonetic" maxlength="30">
-						メイ <input type="text" id="phonetic" name="phonetic" maxlength="30">
-					</td>
-				</tr>
-				<tr>
-					<th>郵便番号</th>	
-					<td>
-						<p>〒 <input type="text" id="address1" name="address1" maxlength="3"> - <input type="text" id="address2" name="address2" maxlength="4"></p>
-					</td>
-				</tr>
-				<tr>
-					<th>住所</th>
-					<td>
-						<p><select id="pref" name="pref"><?php include("includes/pref.html"); ?></select></p>
-						<p><input type="text" id="city" name="city"><br>市区町村名</p>
-						<p><input type="text" id="add" name="add"><br>町域番地</p>
-					</td>
-				</tr>
-				<tr>
-					<th>電話番号</th>
-					<td>
-						<input type="text" id="phone-number" name="phone-number" maxlength="3"> - 
-						<input type="text" id="phone-number" name="phone-number" maxlength="4"> - 
-						<input type="text" id="phone-number" name="phone-number" maxlength="4">
-					</td>
-				</tr>
-				<tr>
-					<th>メールアドレス</th>
-					<td>
-						<p><input type="email" id="email" name="email"></p>
-						<input type="email" id="confirm-email" name="confirm-email">
-						<p>確認のためもう一度入力してください。</p>
-					</td>
-				</tr>
-				<tr>
-					<th>性別</th>
-					<td>
-						<input type="radio" id="sex" name="sex">男性 <input type="radio" id="sex" name="sex">女性
-					</td>
-				</tr>
-				<tr>
-					<th>生年月日</th>
-					<td>
-						<?php include("includes/birthday.html"); ?>
-					</td>
-				</tr>
-				<tr>
-					<th>パスワード</th>
-					<td>
-						<p><input type="password" id="password" name="password" maxlength="15"></p>
-						<input type="password" id="confirm-password" name="confirm-password" maxlength="15">
-						<p>確認のためもう一度入力してください。</p>
-					</td>
-				</tr>
-				<tr>
-					<th>メールマガジンの送付について</th>
-					<td>
-						<p><input type="radio" id="mailmagazine" name="mailmagazine">受け取る</p>
-						<p><input type="radio" id="mailmagazine" name="mailmagazine">受け取らない</p>
-					</td>
-				</tr>
-			</table>
-			<input type="submit" id="submit" name="submit" value="送信">
-		</form>
-	</main>
-	<?php include("includes/top.html"); ?>
-	<?php include("includes/footer.html"); ?>
+	<?php
+	include("includes/connect_DB.php");
+	$first_name = $_REQUEST['first_name']; //FIRST_NAME
+	$last_name = $_REQUEST['last_name']; //LAST_NAME
+	$f_phonetic = $_REQUEST['first_phonetic']; //FRIGANA
+	$l_phonetic = $_REQUEST['last_phonetic']; //FRIGANA
+	$phone_number = $_REQUEST['phone_number1'].'-'.$_REQUEST['phone_number2'].'-'.$_REQUEST['phone_number3']; //PHONE_NUMBER
+	$pref = $_REQUEST['pref']; //ADDRESS_STREET1
+	$city = $_REQUEST['city']; //ADDRESS_STREET2
+	$add = $_REQUEST['add']; //ADDRESS_STREET3
+	$zip = $_REQUEST['address1'].'-'.$_REQUEST['address2']; //ZIP_CODE
+	$email = $_REQUEST['email']; //EMAIL_ADDRESS
+	$sex = $_REQUEST['sex']; //SEX
+	$password = $_REQUEST['password']; //PASSWORD
+	$mailmagazine = $_REQUEST['mailmagazine']; //MAIL_OK
+	$birthday = date('Y/M/D',strtotime($_REQUEST['birthday'] . $_REQUEST['month'] . $_REQUEST['day']));
+	// $birthday = $_REQUEST['birthday'].'/'.$_REQUEST['month'].'/'.$_REQUEST['day'];
+	echo $first_name . '<br>';
+	echo $last_name . '<br>';
+	echo $f_phonetic.$l_phonetic.'<br>';
+	echo $phone_number. '<br>';
+	echo $pref.$city.$add.'<br>';
+	echo $zip.'<br>';
+	echo $email.'<br>';
+	echo $sex.'<br>';
+	echo $password.'<br>';
+	echo $mailmagazine.'<br>';
+	echo $birthday . '<br>';
+	?>
 </body>
 </html>
