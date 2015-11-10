963f8e39d23f2c4c256b9f305b1bc7c67ef11886
reiri <yuuri_ol6_6lo@softbank.ne.jp>
2015-11-10 11:53:16 +0900

    更新

--- admin_direct_mail.php
+++ admin_direct_mail.php
@@ -1,5 +1,50 @@
 <?php include("includes/admin_top.html") ?>
+
   <div class="mail-service">
     <h2>ダイレクトメール送信</h2>
   </div>
+
+  <form action="" method="post">
+  	件名<br/>
+  	<input id="subject" placeholder="件名を入力してください" name="meil_to"><br/>
+  	<br/>
+  	本文<br/>
+  	<textarea name="message" cols="60" rows="10" ></textarea>
+
+
+  </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php include("includes/admin_bottom.html") ?>
+
