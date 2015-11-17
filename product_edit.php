44bd1931eb8a357dec39130489e63f24854e5b07
Davis Kemlers <daviskemlers@gmail.com>
2015-11-17 15:42:51 +0900

    css更新

--- product_edit.php
+++ product_edit.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="css/admin.css" media="all">
+  <link rel="stylesheet" type="text/css" href="css/product_edit.css" media="all">
+  <title>管理者ページ</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.8/jquery.form-validator.min.js"></script>
+</head>
 
 <?php
 $ID = $_GET["ID"];
@@ -12,7 +22,7 @@ while($row = $result->fetch_assoc()){
   $productDescription = $row["PRODUCT_DESCRIPTION"];
   $productPrice = $row["PRODUCT_PRICE"];
 }
- ?>
+?>
 <?php
 include("includes/admin_top.html");
 include("includes/error_messages.php");
