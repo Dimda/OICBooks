a7bdee6aa42a0d378a56adbd37d9b9d6ba2ec712
Davis Kemlers <daviskemlers@gmail.com>
2015-11-17 16:15:43 +0900

    up

--- admin_direct_mail.php
+++ admin_direct_mail.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="css/admin.css" media="all">
+  <link rel="stylesheet" type="text/css" href="css/product_edit.css" media="all">
+  <title>管理者ページ</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.2.8/jquery.form-validator.min.js"></script>
+</head>
 <?php include("includes/admin_top.html");
       include("includes/connect_DB.php");
 ?>
@@ -64,7 +74,7 @@
     exit;
   }
 ?>
-  
+
 
 
 
@@ -94,5 +104,3 @@
 
 
 <?php include("includes/admin_bottom.html") ?>
-
-
